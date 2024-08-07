8019513ef3409d5264f26376267bbf67f1451723
caioolops <cmnl@cin.ufpe.br>
2024-08-07 10:57:47 -0300

    andar continuamente + poção

--- sprites.py
+++ sprites.py
@@ -1,17 +1,18 @@
+
 from config import *
 import math
 import random
 from os.path import join
 from os import walk
 
-#Classe dos blocos de colisão, o jogador não pode passar por eles e eles são invisíveis. 
+
+
 class Block(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
           self.game = game
           self._layer = GROUND_LAYER
           self.groups = self.game.all_sprites, self.game.blocks
           pygame.sprite.Sprite.__init__(self, self.groups)
-          #Multiplico pelo TILESIZE para que cada um esteja disposto a cada 64 pixels (ou o tilesize definido) e assim, seja desenhado certinho com o uso do tilemap. 
           self.x = x * TILESIZE
           self.y = y * TILESIZE
           self.width = TILESIZE
@@ -22,7 +23,8 @@ class Block(pygame.sprite.Sprite):
           self.rect.x = self.x
           self.rect.y = self.y
 
-#Classe do chão
+          self.hitbox = pygame.Rect(self.rect.x, self.rect.y, self.rect.width, self.rect.height)
+
 class Chao(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
           self.game = game
@@ -40,7 +42,46 @@ class Chao(pygame.sprite.Sprite):
           self.rect.x = self.x
           self.rect.y = self.y
 
-#Classe dos itens coletáveis
+class Hitbox(pygame.sprite.Sprite):
+     def __init__(self, game, x, y):
+          self.game = game
+          self._layer = GROUND_LAYER
+          self.groups = self.game.all_sprites, self.game.ground
+          pygame.sprite.Sprite.__init__(self, self.groups)
+          self.x = x * TILESIZE
+          self.y = y * TILESIZE
+          self.width = TILESIZE
+          self.height = TILESIZE
+
+
+          self.rect = self.game.player.rect
+          self.image = pygame.Surface([self.rect.width, self.rect.height])
+          self.image.fill('blue')
+
+     def update(self):
+          self.rect.x = self.game.player.rect.x
+          self.rect.y = self.game.player.rect.y
+
+class HitboxInimigo(pygame.sprite.Sprite):
+     def __init__(self, game, x, y):
+          self.game = game
+          self._layer = GROUND_LAYER
+          self.groups = self.game.all_sprites, self.game.ground
+          pygame.sprite.Sprite.__init__(self, self.groups)
+          self.x = x * TILESIZE
+          self.y = y * TILESIZE
+          self.width = TILESIZE
+          self.height = TILESIZE
+
+
+          self.rect = self.game.inimigo.rect
+          self.image = pygame.Surface([self.rect.width, self.rect.height])
+          self.image.fill('red')
+
+     def update(self):
+          self.rect.x = self.game.inimigo.rect.x
+          self.rect.y = self.game.inimigo.rect.y
+
 class Item(pygame.sprite.Sprite):
      def __init__(self, game, x, y, nome_item):
           self.game = game
@@ -58,7 +99,6 @@ class Item(pygame.sprite.Sprite):
           self.rect.x = self.x
           self.rect.y = self.y
 
-#Classe da imagem das barras dispostas no mapa 
 class Barra(pygame.sprite.Sprite):
      def __init__(self, game, x, y):
           self.game = game
